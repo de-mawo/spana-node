97d49a7de949705e188cb976c19cbd8a4e91c338
de-mawo <groovebassist1@gmail.com>
2024-02-07 17:09:06 +0200

    Added Save User to DB

--- src/utils/passport.ts
+++ src/utils/passport.ts
@@ -1,7 +1,40 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy, Profile } from 'passport-google-oauth20';
+import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
 
+const extractEmail = (profile: Profile): string | null => {
+  if (profile.emails && profile.emails.length > 0) {
+    return profile.emails[0].value;
+  } else {
+    return null;
+  }
+};
 
+const extractImage = (profile: Profile): string | null => {
+  if (profile.photos && profile.photos.length > 0) {
+    return profile.photos[0].value;
+  } else {
+    return null;
+  }
+};
+
+type User = {
+  id?: string;
+};
+
+passport.serializeUser((user: User, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const checkUser = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    done(null, checkUser);
+  } catch (error) {}
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,11 +42,36 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       callbackURL: "/auth/google/callback",
-    //   passReqToCallback: true,
+      //   passReqToCallback: true,
     },
-    function ( accessToken, refreshToken, profile, cb ) {
-        console.log(profile);
-        
+    async function (accessToken, refreshToken, profile, done) {
+      const email = extractEmail(profile) as string;
+      const image = extractImage(profile);
+
+      try {
+        const userExist = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!userExist) {
+          const newUser = await prisma.user.create({
+            data: {
+              name: profile.displayName,
+              email,
+              image,
+              googleId: profile.id,
+            },
+          });
+          done(null, newUser);
+        } else {
+          done(null, userExist);
+        }
+      } catch (error) {
+        // Handle error
+        console.error(error);
+      }
     }
   )
 );
